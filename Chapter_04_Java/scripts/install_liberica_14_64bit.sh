3959b7e172fad9989320dcc70ec5156d9ad896e3
Frank Delporte <frank@webtechie.be>
2020-04-08 09:34:21 +0200

    Removed as 64bit is not a good alternative at this moment

--- Chapter_04_Java/scripts/install_liberica_14_64bit.sh
+++
@@ -1,5 +0,0 @@
-cd /home/pi
-wget http://download.bell-sw.com/java/14+36/bellsoft-jdk14+36-linux-aarch64-full.deb
-sudo apt-get install ./bellsoft-jdk14+36-linux-aarch64-full.deb
-sudo update-alternatives --config javac
-sudo update-alternatives --config java
