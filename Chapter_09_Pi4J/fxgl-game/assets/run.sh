6f09736c9aaf6b547f6313875711bf85b6f97fda
Frank Delporte <fdelporte@azul.com>
2024-02-27 11:22:39 +0100

    Bump FX version

--- Chapter_09_Pi4J/fxgl-game/assets/run.sh
+++ Chapter_09_Pi4J/fxgl-game/assets/run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 java \
   -Dglass.platform=gtk \
-  -Djava.library.path=/opt/javafx-sdk-21/lib \
+  -Djava.library.path=/opt/javafx-sdk/lib \
   -Dmonocle.platform.traceConfig=false \
   -Dprism.verbose=false \
   -Djavafx.verbose=false \
-  --module-path .:/opt/javafx-sdk-21/lib \
+  --module-path .:/opt/javafx-sdk/lib \
   --add-modules javafx.controls \
   --module be.webtechie.fxgl/be.webtechie.fxgl.GameApp $@
