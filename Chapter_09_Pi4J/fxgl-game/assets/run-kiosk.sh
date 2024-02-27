4915ae7c85aee67f5e292dde16747850be772ea5
Frank Delporte <fdelporte@azul.com>
2024-02-27 11:16:43 +0100

    Bump FX version

--- Chapter_09_Pi4J/fxgl-game/assets/run-kiosk.sh
+++ Chapter_09_Pi4J/fxgl-game/assets/run-kiosk.sh
@@ -3,13 +3,13 @@
 export ENABLE_GLUON_COMMERCIAL_EXTENSIONS=true
 java \
   -Degl.displayid=/dev/dri/card1 \
-  -Dmonocle.egl.lib=/opt/javafx-sdk-17/lib/libgluon_drm-1.1.3.so \
-  -Djava.library.path=/opt/javafx-sdk-17/lib \
+  -Dmonocle.egl.lib=/opt/javafx-sdk-21/lib/libgluon_drm-1.1.3.so \
+  -Djava.library.path=/opt/javafx-sdk-21/lib \
   -Dmonocle.platform.traceConfig=false \
   -Dprism.verbose=false \
   -Djavafx.verbose=false \
   -Dmonocle.platform=EGL \
-  --module-path .:/opt/javafx-sdk-17/lib \
+  --module-path .:/opt/javafx-sdk-21/lib \
   --add-modules javafx.controls \
   --module be.webtechie.fxgl/be.webtechie.fxgl.GameApp $@
 /sbin/init 5
--- Chapter_09_Pi4J/fxgl-game/assets/run.sh
+++ Chapter_09_Pi4J/fxgl-game/assets/run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 java \
   -Dglass.platform=gtk \
-  -Djava.library.path=/opt/javafx-sdk-17/lib \
+  -Djava.library.path=/opt/javafx-sdk-21/lib \
   -Dmonocle.platform.traceConfig=false \
   -Dprism.verbose=false \
   -Djavafx.verbose=false \
-  --module-path .:/opt/javafx-sdk-17/lib \
+  --module-path .:/opt/javafx-sdk-21/lib \
   --add-modules javafx.controls \
   --module be.webtechie.fxgl/be.webtechie.fxgl.GameApp $@
