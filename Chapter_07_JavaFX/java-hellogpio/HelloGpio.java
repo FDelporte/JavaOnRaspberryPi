cc9af7713b05e751d8e0c13fba6114789b69bbf3
Frank Delporte <frank@webtechie.be>
2025-12-08 12:20:53 +0100

    Updating examples and READMEs

--- Chapter_07_JavaFX/java-hellogpio/HelloGpio.java
+++ Chapter_07_JavaFX/java-hellogpio/HelloGpio.java
@@ -5,14 +5,12 @@ public class HelloGpio {
         System.out.println("Hello Gpio");
 
         try {
-            Runtime.getRuntime().exec("gpio mode 3 out");
-
             var loopCounter = 0;
             var on = true;
 
             while (loopCounter < 10) {
                 System.out.println("Changing LED to " + (on ? "on" : "off"));
-                Runtime.getRuntime().exec("gpio write 3 " + (on ? "1" : "0"));
+                Runtime.getRuntime().exec("gpioset -c gpiochip0 -t0 23=" + (on ? "1" : "0"));
 
                 on = !on;
 
