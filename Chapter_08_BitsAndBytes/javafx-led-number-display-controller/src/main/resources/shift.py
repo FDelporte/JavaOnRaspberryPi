3667a40d123f6c6bf917d3420428702713edc2a3
Frank Delporte <frank@webtechie.be>
2025-12-08 15:26:05 +0100

    Updating examples and READMEs

--- Chapter_08_BitsAndBytes/javafx-led-number-display-controller/src/main/resources/shift.py
+++ Chapter_08_BitsAndBytes/javafx-led-number-display-controller/src/main/resources/shift.py
@@ -30,33 +30,38 @@ if len(sys.argv) > 1:
 
 print("Sending value: " + str(inputValue))
 
-# Set the RELEASE pin low so the values are stored in memory
-latch_pin.off()
-time.sleep(WAIT_TIME)
+# Use context managers to automatically close pins when done
+with OutputDevice(PIN_DATA) as data_pin, \
+        OutputDevice(PIN_SRCLK) as clock_pin, \
+        OutputDevice(PIN_RCLK_LATCH) as latch_pin:
 
-for y in range(8):
-  # Set the DATA pin to the bit y of the given value
-  bit = inputValue >> (7 - y) & 1
-  print("Sending bit value " + str(bit) + " to slot " + str(y))
+    # Set the RELEASE pin low so the values are stored in memory
+    latch_pin.off()
+    sleep(WAIT_TIME)
 
-  # Prepare for the next value by setting the CLOCK pin LOW
-  clock_pin.off()
-  sleep(WAIT_TIME)
+    for y in range(8):
+        # Set the DATA pin to the bit y of the given value
+        bit = inputValue >> (7 - y) & 1
+        print("Sending bit value " + str(bit) + " to slot " + str(y))
 
-  # Set the data (high or low state) for the pin y
-  if bit:
-      data_pin.on()
-  else:
-      data_pin.off()
-  sleep(WAIT_TIME)
+        # Prepare for the next value by setting the CLOCK pin LOW
+        clock_pin.off()
+        sleep(WAIT_TIME)
 
-  # Set the CLOCK pin HIGH to store the DATA value into memory
-  clock_pin.on()
-  sleep(WAIT_TIME)
+        # Set the data (high or low state) for the pin y
+        if bit:
+            data_pin.on()
+        else:
+            data_pin.off()
+        sleep(WAIT_TIME)
 
-# Loop is finished, so all 8 values are sent
-# Set the RELEASE pin high so the values from memory are sent to the outputs
-latch_pin.on()
-sleep(WAIT_TIME)
+        # Set the CLOCK pin HIGH to store the DATA value into memory
+        clock_pin.on()
+        sleep(WAIT_TIME)
 
-print("Done")
+    # Loop is finished, so all 8 values are sent
+    # Set the RELEASE pin high so the values from memory are sent to the outputs
+    latch_pin.on()
+    sleep(WAIT_TIME)
+
+    print("Done")
